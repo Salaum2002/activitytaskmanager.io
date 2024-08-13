45e93ab0dd841b09040a38800b14f06ae7d3d825
Salaum2002 <adrianvashs.salaum@gmail.com>
2024-08-14 06:44:40 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    for (const link of links) {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            const targetId = link.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    }
+});
